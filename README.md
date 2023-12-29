e5d3181ae11fc94f7c4c09b4fe59c8a90179d50e
mnelson711 <mollyynelson@gmail.com>
2023-12-29 14:54:21 -0500

    added language switch page + readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # codeExplainationWebsite
 A website using Node.js and the openAI API to explain entered code snippets from user.
+
+Add API Key to server.js line 17
+To run: type 'node server.js' in terminal window
--- server.js
+++ server.js
@@ -14,8 +14,16 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //PRIVATE KEY
-API_KEY = "API-KEY-HERE";
+API_KEY = "";
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/switch", (req, res) => {
+  res.sendFile(__dirname + "/transfer.html");
+});
+
+app.get("/explain", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
@@ -31,7 +39,7 @@ app.post("/submit", async (req, res) => {
       messages: [
         {
           role: "user",
-          content: `In the programming language: ${language}, What does the following bit of code mean/do: ${code}. Please provide a detailed response`,
+          content: `In the programming language: ${language}, What does the following bit of code mean/do: ${code}. Please provide a response that is a valid string.`,
         },
       ],
       model: "gpt-3.5-turbo",
@@ -41,6 +49,33 @@ app.post("/submit", async (req, res) => {
     console.log("response: ", responseText);
     res.send(responseText);
 
+  } catch (error) {
+    console.error("Error calling OpenAI API:", error.message);
+    res.status(500).send("Error calling OpenAI API");
+  }
+});
+
+app.post("/switch", async (req, res) => {
+  const { oldlanguage, newlanguage, code } = req.body;
+
+  try {
+    const openai = new OpenAI({
+      apiKey: API_KEY,
+    });
+
+    const openaiResponse = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: `I have code in the programming language: ${oldlanguage}. Could you transfer the code in the language ${newlanguage} so that is the same as the old but in the new language. The code is as follows: ${code} . Please just provide me with the new code and no explainations.`,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const responseText = openaiResponse.choices[0].message.content;
+    console.log("response: ", responseText);
+    res.send(responseText);
   } catch (error) {
     console.error("Error calling OpenAI API:", error.message);
     res.status(500).send("Error calling OpenAI API");
